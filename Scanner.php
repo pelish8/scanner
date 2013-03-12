d31fcd3174282117f4ad5a6db4ecc03792a2c815
Aleksandar Stevic <pelish8@gmail.com>
2013-03-12 21:10:45 +0100

    added scanUpToCharacterFromString

--- Scanner.php
+++ Scanner.php
@@ -4,46 +4,46 @@ namespace pelish8\scanner;
 class Scanner {
 
     /**
-     * The string to scan.
-     *
-     * @var string
-     * @access private
-     */
+    * The string to scan.
+    *
+    * @var string
+    * @access private
+    */
     private $string;
 
     /**
-     * Current position in string.
-     *
-     * @var int
-     * @access private
-     */
+    * Current position in string.
+    *
+    * @var int
+    * @access private
+    */
     private $location = 0;
 
     /**
-     * Total length of the scanned string.
-     *
-     * @var int
-     * @access private
-     */
+    * Total length of the scanned string.
+    *
+    * @var int
+    * @access private
+    */
     private $end = 0;
 
     /**
-     * Constructor.
-     *
-     * @param string $string The string to scan.
-     * @access public
-     */
+    * Constructor.
+    *
+    * @param string $string The string to scan.
+    * @access public
+    */
     public function __construct ($string) {
         $this->string = $string;
         $this->end = strlen($string);
     }
 
     /**
-     * Scan to the string for which to scan, and iclude that string.
-     *
-     * @param string $string The string for which to scan.
-     * @access public
-     */
+    * Scans to the string for which to scan, and include that string.
+    *
+    * @param string $string The string for which to scan.
+    * @access public
+    */
     public function scanToString ($string) {
         $location = strpos($this->string, $string, $this->location);
         
@@ -55,11 +55,11 @@ class Scanner {
     }
 
     /**
-     * Scan to the string for which to scan, and iclude that string.
-     *
-     * @param string $string The string for which to scan.
-     * @access public
-     */
+    * Scans to the string for which to scan, and do not include that string.
+    *
+    * @param string $string The string for which to scan.
+    * @access public
+    */
     public function scanUpToString ($string) {
         $location = strpos($this->string, $string, $this->location);
 
@@ -71,38 +71,51 @@ class Scanner {
     }
 
     /**
-     * Return scanner location in string.
-     *
-     * @access public
-     */
+    * Return scanner location in string.
+    *
+    * @access public
+    */
     public function location () {
         return $this->location;
     }
 
     /**
-     * Check if the scaner is at end.
-     *
-     * @access public
-     */
+    * Check if the scanner is at end.
+    *
+    * @access public
+    */
     public function isAtEnd () {
         return ($this->location >= $this->end);
     }
 
     /**
-     * Set new location from where to start scanning.
-     *
-     * @access public
-     */
+    * Set new location from where to start scanning.
+    *
+    * @param int $location New location form witch to start scanning.
+    * @access public
+    */
     public function setLocation ($location) {
         $this->location = $location;
     }
 
     /**
-     * Returns the string with which the receiver was created.
-     *
-     * @access public
-     */
+    * Returns the string with which the receiver was created.
+    *
+    * @access public
+    */
     public function string () {
         return $this->string;
     }
+    
+    /**
+    * Scans the string until a character from a given string is encountered.
+    * 
+    * @param string $charactersSet The characters up to which to scan.
+    *
+    */
+    public function scanUpToCharacterFromString ($charactersString) {
+        $location = strcspn($this->string, $charactersString, $this->location, $this->end - $this->location);
+        $this->location += $location;
+    }
+    
 }
